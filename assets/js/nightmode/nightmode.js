aadc26479558bdd6898f46b6bdefd33a3edf105a
kresna <kresnafti2013@gmail.com>
2023-07-05 23:30:28 +0700

    fix footer night mode

--- assets/js/nightmode/nightmode.js
+++ assets/js/nightmode/nightmode.js
@@ -1,1 +1,28 @@
-const darkMode=e=>{if(e){let e=document.getElementById("nightmode-mobile"),d=document.getElementById("header");1==e.checked?(document.body.classList.add("dark-mode"),d.classList.add("dark-mode"),localStorage.setItem("nightmode",!0),document.getElementById("nightmode").checked=!0):(document.body.classList.remove("dark-mode"),d.classList.remove("dark-mode"),localStorage.removeItem("nightmode"),document.getElementById("nightmode").checked=!1)}else{let e=document.getElementById("header");1==document.getElementById("nightmode").checked?(document.body.classList.add("dark-mode"),e.classList.add("dark-mode"),localStorage.setItem("nightmode",!0),document.getElementById("nightmode-mobile").checked=!0,document.getElementById("img-toggle-dark").style.display="none",document.getElementById("img-toggle-light").style.display="block"):(document.body.classList.remove("dark-mode"),e.classList.remove("dark-mode"),localStorage.removeItem("nightmode"),document.getElementById("nightmode-mobile").checked=!1,document.getElementById("img-toggle-dark").style.display="block",document.getElementById("img-toggle-light").style.display="none")}};
+const darkMode = (e) => {
+  if (e) {
+      let e = document.getElementById("nightmode-mobile"),
+          d = document.getElementById("header");
+      1 == e.checked
+          ? (document.body.classList.add("dark-mode"), d.classList.add("dark-mode"), localStorage.setItem("nightmode", !0), (document.getElementById("nightmode").checked = !0))
+          : (document.body.classList.remove("dark-mode"), d.classList.remove("dark-mode"), localStorage.removeItem("nightmode"), (document.getElementById("nightmode").checked = !1));
+  } else {
+      let e = document.getElementById("header");
+      let footer = document.getElementById("footer");
+      1 == document.getElementById("nightmode").checked
+          ? (
+            document.body.classList.add("dark-mode"),
+            e.classList.add("dark-mode"),
+            footer.classList.add("dark-mode"),
+            localStorage.setItem("nightmode", !0),
+            (document.getElementById("nightmode-mobile").checked = !0),
+            (document.getElementById("img-toggle-dark").style.display = "none"),
+            (document.getElementById("img-toggle-light").style.display = "block"))
+          : (document.body.classList.remove("dark-mode"),
+            e.classList.remove("dark-mode"),
+            footer.classList.remove("dark-mode"),
+            localStorage.removeItem("nightmode"),
+            (document.getElementById("nightmode-mobile").checked = !1),
+            (document.getElementById("img-toggle-dark").style.display = "block"),
+            (document.getElementById("img-toggle-light").style.display = "none"));
+  }
+};
