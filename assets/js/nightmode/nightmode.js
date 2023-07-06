b0ff34edf865b871182868e608dec911bd75b039
kresna <kresnafti2013@gmail.com>
2023-07-06 19:40:57 +0700

    fix color and responsive footer

--- assets/js/nightmode/nightmode.js
+++ assets/js/nightmode/nightmode.js
@@ -2,9 +2,11 @@ const darkMode = (e) => {
   if (e) {
       let e = document.getElementById("nightmode-mobile"),
           d = document.getElementById("header");
+
+      let footer = document.getElementById("footer");
       1 == e.checked
-          ? (document.body.classList.add("dark-mode"), d.classList.add("dark-mode"), localStorage.setItem("nightmode", !0), (document.getElementById("nightmode").checked = !0))
-          : (document.body.classList.remove("dark-mode"), d.classList.remove("dark-mode"), localStorage.removeItem("nightmode"), (document.getElementById("nightmode").checked = !1));
+          ? (document.body.classList.add("dark-mode"), d.classList.add("dark-mode"), footer.classList.add("dark-mode"), localStorage.setItem("nightmode", !0), (document.getElementById("nightmode").checked = !0))
+          : (document.body.classList.remove("dark-mode"), d.classList.remove("dark-mode"), footer.classList.remove("dark-mode"), localStorage.removeItem("nightmode"), (document.getElementById("nightmode").checked = !1));
   } else {
       let e = document.getElementById("header");
       let footer = document.getElementById("footer");
